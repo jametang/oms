d1e970c2e84992ca091124ff9b99b946c8673e55
tangli <18782994956@163.com>
2023-12-03 23:02:13 +0800

    Revert "modify pom.xml with activiti"
    This reverts commit 4f0958b78a88fb049913fe29eb66ec5e25d501c8.

--- src/main/java/com/zjjc/oms/WorkflowConfig.java
+++ src/main/java/com/zjjc/oms/WorkflowConfig.java
@@ -1,8 +1,6 @@
 package com.zjjc.oms;
 
 import jakarta.annotation.PostConstruct;
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
 import org.activiti.spring.ProcessEngineFactoryBean;
 import org.activiti.spring.SpringProcessEngineConfiguration;
@@ -16,15 +14,13 @@ import java.util.Objects;
 @Configuration
 public class WorkflowConfig {
 
-
+/*    private RepositoryService repositoryService;
     @PostConstruct
     public void init(){
-        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
-        RepositoryService repositoryService = processEngine.getRepositoryService();
         repositoryService.createDeployment().addClasspathResource("bpmn/order-process.bpmn20.xml");
-    }
+    }*/
 
-/*    @Bean
+    @Bean
     public SpringProcessEngineConfiguration springProcessEngineConfiguration(DataSource dataSource) {
         SpringProcessEngineConfiguration configuration = new SpringProcessEngineConfiguration();
         configuration.setDataSource(dataSource);
@@ -42,6 +38,6 @@ public class WorkflowConfig {
     @Bean
     public RepositoryService repositoryService(ProcessEngineFactoryBean processEngineFactoryBean) throws Exception {
         return Objects.requireNonNull(processEngineFactoryBean.getObject()).getRepositoryService();
-    }*/
+    }
 
 }
